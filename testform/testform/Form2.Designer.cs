c22299991d41c65fcb00d14d549513847e3a83a6
dohyeon ji <gus3826@naver.com>
2019-07-11 18:36:22 +0900

    ㅁㄴㅁ

--- testform/testform/Form2.Designer.cs
+++ testform/testform/Form2.Designer.cs
@@ -32,6 +32,7 @@
             this.eventLog1 = new System.Diagnostics.EventLog();
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -66,11 +67,21 @@
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(441, 121);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkedListBox1);
@@ -88,5 +99,6 @@
         private System.Diagnostics.EventLog eventLog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
