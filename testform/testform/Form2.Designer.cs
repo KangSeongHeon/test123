2c513aa2ceb96e1766f7436fca283615e0255ad6
dohyeon ji <gus3826@naver.com>
2019-07-11 18:14:11 +0900

    버튼 추가

--- testform/testform/Form2.Designer.cs
+++ testform/testform/Form2.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -39,14 +42,29 @@
             this.checkedListBox1.Size = new System.Drawing.Size(134, 212);
             this.checkedListBox1.TabIndex = 0;
             // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 296);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkedListBox1);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,5 +72,7 @@
         #endregion
 
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Button button1;
     }
 }
